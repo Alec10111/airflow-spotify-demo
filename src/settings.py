d6694a20cb152e61ba0a1ad718b39a023ecb9b24
Carlos Fernandez <cafl.23w@gmail.com>
2023-06-17 00:59:13 +0200

    add functionality to dynamically get spotify token

--- src/settings.py
+++ src/settings.py
@@ -4,4 +4,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 X_RAPIDAPI_KEY = os.getenv('X_RAPIDAPI_KEY')
-SPOTIFY_ACCESS_TOKEN = os.getenv('SPOTIFY_ACCESS_TOKEN')
+SPOTIFY_CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
+SPOTIFY_CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
--- src/spotify.py
+++ src/spotify.py
@@ -2,11 +2,17 @@ import requests
 import settings
 
 BASE_URL = "https://api.spotify.com/v1"
-headers = {"Authorization": f"Bearer {settings.SPOTIFY_ACCESS_TOKEN}"}
+headers = {"Authorization": f"Bearer {get_token()}"}
 
-PLAYLIST_ID = "4l351m3ZtXc2ir88426v99"
+def get_token():
+    header = {"Content-Type: application/x-www-form-urlencoded"}
+    response = requests.post(
+        url="https://accounts.spotify.com/api/token", 
+        data="grant_type=client_credentials&client_id={settings.SPOTIFY_CLIENT_ID}&client_secret={settings.SPOTIFY_CLIENT_SECRET}",
+        headers=header
+        ).json()
+    return response["access_token"]
 
-top_tracks = ['7z3Q4j4HJDSOZtbR5rbYdG','04wmRKlU8lSjmYql1mt052','6jemILri7l5LuPhKs2nuXr','0vber253Le8WWt7gKorVe9','6Kc9FbBnrzq0AbKEPfNkMx']
 
 
 def get_track_id(song_name: str, artist: str) -> str:
@@ -23,8 +29,10 @@ def get_playlist_id(playlist_name: str) -> str:
 
 
 def get_recommended_tracks():
+    top_tracks = ['7z3Q4j4HJDSOZtbR5rbYdG','04wmRKlU8lSjmYql1mt052','6jemILri7l5LuPhKs2nuXr','0vber253Le8WWt7gKorVe9','6Kc9FbBnrzq0AbKEPfNkMx']
     url = f"{BASE_URL}/recommendations?limit=5&seed_tracks=${','.join(top_tracks)}"
 
 def update_playlist(song_list: list):
+    PLAYLIST_ID = "4l351m3ZtXc2ir88426v99"
     url = f"{BASE_URL}/v1/playlists/${PLAYLIST_ID}/tracks?uris=${','.join(song_list)}"
-    requests.post(url)
+    requests.post(url,headers=headers)
